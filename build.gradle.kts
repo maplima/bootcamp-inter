8c18db97da72f3fa6ac9a94a1f7ab223c68704a4
Manoel Anivaldo Pereira Lima Filho <manoel.lima@bancointer.com.br>
2021-09-02 17:35:25 -0300

    Etapa 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:${Deps.Version.agp}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.Version.kotlin}")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    id("my-project-plugin")
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,33 @@ plugins {
 }
 
 repositories {
+    google()
     mavenCentral()
+}
+
+kotlin {
+    // Add Deps to compilation, so it will become available in main project
+    sourceSets.getByName("main").kotlin.srcDir("buildSrc/src/main/kotlin")
+}
+
+gradlePlugin {
+    plugins {
+        register("my-project-plugin") {
+            id = "my-project-plugin"
+            implementationClass = "ProjectPlugin"
+        }
+        register("my-library-plugin") {
+            id = "my-library-plugin"
+            implementationClass = "LibraryPlugin"
+        }
+        register("my-application-plugin") {
+            id = "my-application-plugin"
+            implementationClass = "ApplicationPlugin"
+        }
+    }
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:${Deps.Version.agp}")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.Version.kotlin}")
 }
