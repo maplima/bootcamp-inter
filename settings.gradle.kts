6280c354035e1f94bb78302ad9a6ec0739b1c782
Manoel Anivaldo Pereira Lima Filho <manoel.lima@bancointer.com.br>
2021-09-02 18:14:45 -0300

    Etapa 4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
 rootProject.name = "Bootcamp Inter"
 include(":app")
 include(":module1")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+}
+
+includeBuild("includedBuilds/build-configuration")
+includeBuild("includedBuilds/build-source")
