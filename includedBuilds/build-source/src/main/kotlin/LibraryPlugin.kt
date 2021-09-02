a706e6fe13ea8da8e1809843bde17ea28ac0da94
Manoel Anivaldo Pereira Lima Filho <manoel.lima@bancointer.com.br>
2021-09-02 18:20:58 -0300

    Etapa 4 - Ajuste

--- includedBuilds/build-source/src/main/kotlin/LibraryPlugin.kt
+++ includedBuilds/build-source/src/main/kotlin/LibraryPlugin.kt
@@ -63,7 +63,7 @@ fun Project.applyAndroid() {
 
 fun Project.applyDependencies() {
     dependencies {
-        "implementation"("org.jetbrains.kotlin:kotlin-stdlib:4.2.2")
+        "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Deps.Version.kotlin}")
         "implementation"("androidx.core:core-ktx:1.6.0")
         "implementation"("androidx.appcompat:appcompat:1.3.1")
         "implementation"("com.google.android.material:material:1.4.0")
